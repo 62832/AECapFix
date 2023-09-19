a7d465cba6d33265321932b59067d18fc5a6aa62
90 <hi@90.gripe>
2023-09-19 14:11:16 +0100

    Add forgotten super call

--- src/main/java/gripe/_90/aecapfix/mixin/SkyStoneTankBlockEntityMixin.java
+++ src/main/java/gripe/_90/aecapfix/mixin/SkyStoneTankBlockEntityMixin.java
@@ -23,6 +23,7 @@ public abstract class SkyStoneTankBlockEntityMixin extends BlockEntity {
 
     @Override
     public void invalidateCaps() {
+        super.invalidateCaps();
         holder.invalidate();
     }
 }
